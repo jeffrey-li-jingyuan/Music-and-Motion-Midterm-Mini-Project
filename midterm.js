8ee5559f40e38c7c9607365e75de460c0379afcc
jeffrey-li-jingyuan <47227251+jeffrey-li-jingyuan@users.noreply.github.com>
2019-03-17 22:36:46 -0700

    Create midterm.js

---
+++ midterm.js
@@ -0,0 +1,26 @@
+var mic;
+var vol;
+function setup() {
+  createCanvas(500, 500);
+  //mic input setting
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw() {
+  var vol = mic.getLevel();
+  background(8, 6, 55);
+  fill(10);
+  rect(width/2, height/2 + 30, 400, 440);
+
+  rectMode(CENTER);
+
+  if (vol > 0.4){
+  fill (243, 255, 65);
+}
+  for (let i = 100; i <= width - 60; i += 50){
+    for (let j = 130; j <= height; j += 70){
+    rect(i, j, 30, 50)
+    }
+  }
+}
